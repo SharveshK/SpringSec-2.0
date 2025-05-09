6bea549e048a650f8bb2728fb91a61294701c7c0
Sharvesh <karthiksharvesh6@gmail.com>
2025-05-09 11:50:33 +0530

    Initial Login Page without all the fields

---
+++ src/main/java/com/sk/SpringSec/repo/UserRepo.java
@@ -0,0 +1,11 @@
+package com.sk.SpringSec.repo;
+
+import com.sk.SpringSec.model.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<Users, Integer> {
+
+    Users findByUsername(String username);
+}
